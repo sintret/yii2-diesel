719d86c51bdee5cb658a54ff26f460a74a0711c4
andy fitria <sintret@gmail.com>
2016-09-14 15:28:58 +0700

    message:

---
+++ controllers/Controller.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace sintret\diesel\controllers;
+
+use Yii;
+use yii\web\Controller;
+use yii\helpers\Url;
+
+
+/**
+ * CController base on all access controllers dynamic
+ */
+class Controller extends Controller {
+
+    public $user;
+
+    public function init() {
+        parent::init();
+    }
+
+    public function beforeAction($action) {
+        $actionId = $action->id;
+        $method = strtolower(Yii::$app->controller->id);
+        $actionMethod = $method . '.' . $actionId;
+
+        if (in_array($actionMethod, self::fieldsArray())) {
+            if ($this->accessMenu($actionMethod))
+                return true;
+            else {
+                if (Yii::$app->user->id) {
+                    $this->redirect(Url::to(['webadmin/403']));
+                    return false;
+                    exit(0);
+                } else {
+                    $this->redirect(Url::to(['site/login']));
+                }
+            }
+        } else {
+            $this->redirect(Url::to(['site/login']));
+        }
+        parent::beforeAction($action);
+    }
+
+    public function accessMenu($name) {
+        
+        if (Yii::$app->user->id) {
+            if (Yii::$app->user->id == -1)
+                return true;
+            else
+                return self::checkAccess($name, Yii::$app->user->identity->roleId);;
+        } else
+            return false;
+    }
+
+    public static function checkAccess($name, $roleId) {
+
+        $parts = explode(".", $name);
+        return \app\models\Access::find()->where([
+                    'roleId' => $roleId,
+                    'LOWER(controller)' => strtolower($parts[0]),
+                    'LOWER(method)' => strtolower($parts[1])])->exists();
+    }
+
+    public static function accessTo($name) {
+        return self::checkAccess($name, Yii::$app->user->identity->roleId);
+    }
+
+    public static function checkManyAccess($array, $roleId) {
+        $return = 0;
+        if ($array)
+            foreach ($array as $v) {
+                if (self::checkAccess($v, $roleId)) {
+                    $return +=1;
+                }
+            }
+
+        return $return;
+    }
+
+    public static function fieldsArray() {
+        $fields = \app\models\Role::accessFilter();
+
+        $return = [];
+        foreach ($fields as $keys => $values) {
+            foreach ($values as $k => $v) {
+                $return[] = strtolower($keys) . '.' . $v;
+            }
+        }
+
+        return $return;
+    }
+
+    public function accessUser($name) {
+        if (Yii::$app->user->id) {
+            $role = \yii\helpers\Json::decode(Yii::$app->user->identity->roles->params);
+        } else
+            return false;
+    }
+
+}
---
+++ generators/model/default/model.php
@@ -0,0 +1,155 @@
+<?php
+/**
+ * This is the template for generating the model class of a specified table.
+ */
+
+/* @var $this yii\web\View */
+/* @var $generator yii\gii\generators\model\Generator */
+/* @var $tableName string full table name */
+/* @var $className string class name */
+/* @var $queryClassName string query class name */
+/* @var $tableSchema yii\db\TableSchema */
+/* @var $labels string[] list of attribute labels (name => label) */
+/* @var $rules string[] list of validation rules */
+/* @var $relations array list of relations (name => relation declaration) */
+
+echo "<?php\n";
+?>
+
+namespace <?= $generator->ns ?>;
+
+use Yii;
+
+/**
+ * This is the model class for table "<?= $generator->generateTableName($tableName) ?>".
+ *
+<?php foreach ($tableSchema->columns as $column): ?>
+ * @property <?= "{$column->phpType} \${$column->name}\n" ?>
+<?php endforeach; ?>
+<?php if (!empty($relations)): ?>
+ *
+<?php foreach ($relations as $name => $relation): ?>
+ * @property <?= $relation[1] . ($relation[2] ? '[]' : '') . ' $' . lcfirst($name) . "\n" ?>
+<?php endforeach; ?>
+<?php endif; ?>
+ */
+class <?= $className ?> extends <?= '\\' . ltrim($generator->baseClass, '\\') . "\n" ?>
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return '<?= $generator->generateTableName($tableName) ?>';
+    }
+<?php if ($generator->db !== 'db'): ?>
+
+    /**
+     * @return \yii\db\Connection the database connection used by this AR class.
+     */
+    public static function getDb()
+    {
+        return Yii::$app->get('<?= $generator->db ?>');
+    }
+<?php endif; ?>
+    
+    <?php 
+    $isImage = false;
+    $viewRule = "";
+    foreach ($labels as $name => $label) {
+        if($name=='image'){
+            $isImage = true;
+            $viewRule .= ", \n";
+            $viewRule .= "\n            [['image'], 'file', 'extensions' => 'jpg,png,gif'],";
+        }
+    }
+    ?>
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [<?= "\n            " . implode(",\n            ", $rules) . $viewRule."\n        "?>];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+<?php foreach ($labels as $name => $label): ?>
+            <?= "'$name' => " . $generator->generateString($label) . ",\n" ?>
+<?php endforeach; ?>
+        ];
+    }
+<?php foreach ($relations as $name => $relation): ?>
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function get<?= $name ?>()
+    {
+        <?= $relation[0] . "\n" ?>
+    }
+<?php endforeach; ?>
+<?php if ($queryClassName): ?>
+<?php
+    $queryClassFullName = ($generator->ns === $generator->queryNs) ? $queryClassName : '\\' . $generator->queryNs . '\\' . $queryClassName;
+    echo "\n";
+?>
+    /**
+     * @inheritdoc
+     * @return <?= $queryClassFullName ?> the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new <?= $queryClassFullName ?>(get_called_class());
+    }
+<?php endif; ?>
+        
+    <?php if ($isImage) { ?>
+    
+    /**
+     * @inheritdoc
+     * This case using image upload and store with easyly.
+     */
+     
+    public static $imagePath = '@webroot/images/<?= strtolower($className) ?>/';
+    
+    public function getImageTrue()
+    {
+        if($this->image){
+            return Yii::getAlias($this->image);
+        }
+    }
+    
+    public function getThumbnailTrue()
+    {
+        if ($this->image) {
+            $name = \yii\helpers\StringHelper::basename($this->image);
+            $dir = \yii\helpers\StringHelper::dirname($this->image);
+            return Yii::getAlias($dir . '/thumb/' . $name);
+        }
+    }
+    
+    public function getThumb()
+    {
+        if ($this->thumbnailTrue) {
+            return \yii\helpers\Html::img($this->thumbnailTrue, ['width' => '100px']);
+        }
+    }
+    
+    public function behaviors()
+    {
+        return [
+            'image' => [
+            'class' => \sintret\gii\components\CropBehavior::className(),
+            'paths' => self::$imagePath . '{id}/',
+            'width'=>200,
+            ]
+        ];
+    }
+    <?php } ?>
+}
